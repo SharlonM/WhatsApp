247f32018ba21b1b81715036ea0f4f2e99b98d19
SalomoniJuliana <salomoni.juliana@gmail.com>
2019-06-20 00:31:48 -0300

    adcionado layouts de Login e Validação

---
+++ app/src/main/java/com/sharlon/whatsapp/ValidadorActivity.java
@@ -0,0 +1,14 @@
+package com.sharlon.whatsapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ValidadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validador);
+    }
+}
