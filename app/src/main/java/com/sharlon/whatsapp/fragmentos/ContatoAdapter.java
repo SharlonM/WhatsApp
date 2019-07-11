635b0d031d20433a08bcf7456592165a5b569c9a
Sharlon M <sharlon12@hotmail.com>
2019-07-11 17:53:50 -0300

    mudança em tudo, preguiça de especificar

---
+++ app/src/main/java/com/sharlon/whatsapp/fragmentos/ContatoAdapter.java
@@ -0,0 +1,50 @@
+package com.sharlon.whatsapp.fragmentos;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.sharlon.whatsapp.R;
+import com.sharlon.whatsapp.modelos.Contatos;
+
+import java.util.ArrayList;
+
+public class ContatoAdapter extends ArrayAdapter<Contatos> {
+
+    private ArrayList<Contatos> contatos;
+    private Context context;
+
+    public ContatoAdapter(Context context, ArrayList<Contatos> objects) {
+        super(context, 0, objects);
+
+        this.contatos = objects;
+        this.context = context;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+
+        View view = null;
+
+        // verificar se a lista esta vazia
+        if (!contatos.isEmpty()) {
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            // montar a view apartir do xml
+            view = inflater.inflate(R.layout.lista_contatos, parent, false);
+
+            // recuperar o elemento para exibiçao
+            TextView nomeContato = view.findViewById(R.id.txt_nome);
+            nomeContato.setText(contatos.get(position).getNome());
+
+        }
+
+        return view;
+    }
+}
--- app/src/main/java/com/sharlon/whatsapp/fragmentos/ContatosFragment.java
+++ app/src/main/java/com/sharlon/whatsapp/fragmentos/ContatosFragment.java
@@ -7,14 +7,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.sharlon.whatsapp.R;
+import com.sharlon.whatsapp.firebase.ConfigFirebase;
+import com.sharlon.whatsapp.modelos.Contatos;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
+
+    private ContatoAdapter adapter;
+    private ListView lista;
+    private ArrayList<Contatos> listaDeContatos;
+    private ValueEventListener valueEventListenerContatos;
 
 
     public ContatosFragment() {
@@ -23,15 +37,74 @@ public class ContatosFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-
         View view = inflater.inflate(R.layout.fragment_contatos, container, false);
-        ListView lista = view.findViewById(R.id.listaContatos);
+
+        lista = view.findViewById(R.id.listaContatos);
+        listaDeContatos = new ArrayList<>();
+
+        recuperarContatos();
+
 
         return view;
     }
 
+    private void recuperarContatos() {
+
+        valueEventListenerContatos = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                listaDeContatos.clear();
+
+                for (DataSnapshot dados : dataSnapshot.getChildren()) {
+
+                    final Contatos c = dados.getValue(Contatos.class);
+                    assert c != null;
+                    listaDeContatos.add(c);
+
+                }
+
+
+//                adapter = new ArrayAdapter<>(
+//                        Objects.requireNonNull(getActivity()),
+//                        android.R.layout.simple_list_item_1,
+//                        listaDeContatos
+//                );
+
+                adapter = new ContatoAdapter(getActivity(), listaDeContatos);
+
+                lista.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        ConfigFirebase.getReferenciaBanco()
+                .child(Objects.requireNonNull(ConfigFirebase.getUser().getPhoneNumber()))
+                .child("Contatos")
+                .addValueEventListener(valueEventListenerContatos);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ConfigFirebase.getReferenciaBanco()
+                .child(Objects.requireNonNull(ConfigFirebase.getUser().getPhoneNumber()))
+                .child("Contatos")
+                .removeEventListener(valueEventListenerContatos);
+    }
 }
