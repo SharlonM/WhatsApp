a3e6a40defb82e3df54ba7794bd0c240d23509fd
Sharlon M <sharlon12@hotmail.com>
2019-07-15 18:06:09 -0300

    Monstrando mensagens brancas quando envia e verde quando recebe.

--- app/src/main/java/com/sharlon/whatsapp/ConversaActivity.java
+++ app/src/main/java/com/sharlon/whatsapp/ConversaActivity.java
@@ -2,14 +2,24 @@ package com.sharlon.whatsapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.sharlon.whatsapp.firebase.ConfigFirebase;
+import com.sharlon.whatsapp.fragmentos.ConversaAdapter;
 import com.sharlon.whatsapp.modelos.Mensagens;
+
+import java.util.ArrayList;
 
 
 public class ConversaActivity extends AppCompatActivity {
@@ -17,6 +27,11 @@ public class ConversaActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private EditText edtMensagem;
     private ImageButton imgEnviar;
+    private ListView listView;
+    private ArrayList<Mensagens> arrayMensagens;
+    private ArrayAdapter<Mensagens> adapter;
+    private DatabaseReference firebase;
+    private ValueEventListener eventListener;
 
 
     @Override
@@ -27,6 +42,7 @@ public class ConversaActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         edtMensagem = findViewById(R.id.edt_mensagem);
         imgEnviar = findViewById(R.id.imgSend);
+        listView = findViewById(R.id.lv_conversas);
 
         if (bundle != null) {
 
@@ -34,6 +50,9 @@ public class ConversaActivity extends AppCompatActivity {
             final String numeroDestinatario = bundle.getString("numero");
             final String historico = bundle.getString("historico");
             String fotoDestinatario = bundle.getString("foto");
+            final String numeroRemetente = ConfigFirebase.getUser().getPhoneNumber();
+
+            //configurar a toolbar
 
             toolbar = findViewById(R.id.toolbarConversas);
 
@@ -42,6 +61,43 @@ public class ConversaActivity extends AppCompatActivity {
             toolbar.setNavigationIcon(R.drawable.ic_action_arrow_left);
             toolbar.setLogo(R.drawable.ic_account_circle_white_36dp);
             setSupportActionBar(toolbar);
+
+            // configurar listview e adapter
+
+            arrayMensagens = new ArrayList<>();
+            adapter = new ConversaAdapter(this, arrayMensagens);
+            listView.setAdapter(adapter);
+
+            // recuperar historico de mensagens
+
+            firebase = ConfigFirebase.getReferenciaBanco().child("Conversas")
+                    .child(numeroRemetente)
+                    .child(numeroDestinatario);
+
+            eventListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+
+                        arrayMensagens.clear();
+
+                        for (DataSnapshot dados : dataSnapshot.getChildren()) {
+                            Mensagens mensagem = dados.getValue(Mensagens.class);
+                            arrayMensagens.add(mensagem);
+                        }
+
+                        adapter.notifyDataSetChanged();
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+
+            firebase.addValueEventListener(eventListener);
 
             imgEnviar.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -52,10 +108,10 @@ public class ConversaActivity extends AppCompatActivity {
                     if (!edtmensagem.isEmpty()) {
 
                         Mensagens mensagens = new Mensagens();
-                        mensagens.setIdUsuario(ConfigFirebase.getUser().getPhoneNumber());
+                        mensagens.setIdUsuario(numeroRemetente);
                         mensagens.setMensagem(edtmensagem);
 
-                        salvarMensagem(ConfigFirebase.getUser().getPhoneNumber(), numeroDestinatario, mensagens);
+                        salvarMensagem(numeroRemetente, numeroDestinatario, mensagens);
 
                         edtMensagem.setText("");
 
@@ -89,5 +145,17 @@ public class ConversaActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebase.removeEventListener(eventListener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebase.addValueEventListener(eventListener);
     }
 }
---
+++ app/src/main/java/com/sharlon/whatsapp/fragmentos/ConversaAdapter.java
@@ -0,0 +1,57 @@
+package com.sharlon.whatsapp.fragmentos;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.sharlon.whatsapp.R;
+import com.sharlon.whatsapp.firebase.ConfigFirebase;
+import com.sharlon.whatsapp.modelos.Mensagens;
+
+import java.util.ArrayList;
+
+public class ConversaAdapter extends ArrayAdapter<Mensagens> {
+
+    private Context c;
+    private ArrayList<Mensagens> mensagens;
+
+    public ConversaAdapter(Context context, ArrayList<Mensagens> objects) {
+        super(context, 0, objects);
+        this.c = context;
+        this.mensagens = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = null;
+
+        if (!mensagens.isEmpty()) {
+
+            // recuperar dados do remetente
+
+            String numeroRementente = ConfigFirebase.getUser().getPhoneNumber();
+
+            // inicializa o objeto
+            LayoutInflater inflater = (LayoutInflater) c.getSystemService(c.LAYOUT_INFLATER_SERVICE);
+
+            // recupera o elemento para exibiçao
+            Mensagens msg = mensagens.get(position);
+
+            if (numeroRementente.equals(msg.getIdUsuario())) {
+
+                view = inflater.inflate(R.layout.item_mensagem_direita, parent, false);
+
+            } else {
+                view = inflater.inflate(R.layout.item_mensagem_esquerda, parent, false);
+            }
+
+            TextView textoMensagem = view.findViewById(R.id.txt_mensagem);
+            textoMensagem.setText(msg.getMensagem());
+        }
+
+        return view;
+    }
+}
